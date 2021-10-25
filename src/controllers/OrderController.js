6b4069276c1be424df35eab6dd8883299268c589
Stickyamp11 <i82cosem@uco.es>
2021-10-25 16:34:10 +0200

    Añadidas funciones al controlador para gestionar lista de pedidos

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,9 +1,11 @@
 const OrderModel = require('../models/OrderModel')
 const OrderItemModel = require('../models/OrderItemModel')
+const ProductModel = require ('../models/ProductModel')
 class OrderController{
 
     static  orderModel = new OrderModel();
     static orderItemModel = new OrderItemModel();
+    static productModel = new ProductModel();
 
     static index = (request, response) => {
         this.orderModel.getAll().then(([data]) => response.send(data));
@@ -18,7 +20,8 @@ class OrderController{
         this.orderModel.create(total, customerId).then(([data]) => {
             if(items != null && data != null && data.insertId != undefined){
                 for (x in items){
-                    this.orderItemModel.create(x.unitPrice, x.quantity, data.insertId, x.productId)
+                    //Añade los productos con la orden a la lista
+                    this.productModel.getById(x.productId).then(([data2]) => this.orderItemModel.create(data2.unitPrice, x.quantity, data.insertId, x.productId))
                 }
             }
             response.send(data)
@@ -35,12 +38,31 @@ class OrderController{
         const total = request.body.total;
         const customerId = request.body.customerId;
         const order = {id,total, customerId}
-        this.orderModel.update(order).then(([data]) => response.send(data))
+
+
+        this.orderModel.update(order).then(([data]) => {
+
+            if(items != null && data != null && data.insertId != undefined){
+                this.OrderItemModel.deleteAll(id) //Elimina la lista de productos de la orden
+               
+                for (x in items){
+                    //Añade los productos con la orden a la lista
+                    this.productModel.getById(x.productId).then(([data2]) => this.orderItemModel.create(data2.unitPrice, x.quantity, data.insertId, x.productId))
+                }
+            }
+            response.send(data)
+        })
     }
 
     static delete = (request, response) => {
         const id = request.params.id;
-        this.orderModel.delete(id).then(([data]) => response.send(data))
+        this.orderModel.delete(id).then(([data]) => {
+            
+            this.OrderItemModel.deleteAll(id) //Elimina la lista de productos de la orden
+
+            response.send(data)
+
+        })
 
     }
 }
