c24b67ab842ed5186eb164a53e3cd0ca47e36218
Stickyamp11 <i82cosem@uco.es>
2021-10-25 11:04:19 +0200

    Pequeño ajuste de renombramiento

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -3,15 +3,17 @@ const OrderModel = require('../models/OrderModel')
 class OrderController{
 
     static  orderModel = new OrderModel();
+    
     static index = (request, response) => {
         this.orderModel.getAll().then(([data]) => response.send(data));
 
     }
 
     static create = (request, response) => {
-        const total = request.body.totalAmount;
+        const total = request.body.total;
         const customerId = request.body.customerId;
         this.orderModel.create(total, customerId).then(([data]) => response.send(data))
+
     }
 
     static read = (request, response) => {
@@ -20,16 +22,17 @@ class OrderController{
     }
 
     static update = (request, response) => {
-        const id = request.params.id;
+        const id = request.params.id;  //Usas params para obtener los parametros de la url (en CategoryRouter es :id)
         const total = request.body.total;
         const customerId = request.body.customerId;
-        const order = {id, total, customerId}
+        const order = {id,total, customerId}
         this.orderModel.update(order).then(([data]) => response.send(data))
     }
 
     static delete = (request, response) => {
         const id = request.params.id;
         this.orderModel.delete(id).then(([data]) => response.send(data))
+
     }
 }
 
