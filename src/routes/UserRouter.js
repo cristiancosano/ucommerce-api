7e77fe8f400bd95d8a03dc5311cb97ad6f9a8379
Cristian Cosano <cristian@MacBook-Air-de-Cristian.local>
2021-10-27 16:58:58 +0200

    Change individual validation methods for single checkSchema validation

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const userController = require('../controllers/UserController');
 const checkToken = require('../middlewares/checkToken');
-const { body, param } = require('express-validator');
+const { body, param, checkSchema } = require('express-validator');
 const validateParams = require('../middlewares/validateParams');
 
 
@@ -17,11 +17,47 @@ router.get('/:id', checkToken, userController.read);
 router.put(
     '/:id',
     checkToken,
-    param('id').isInt(),
-    body('email').isEmail(),
-    body('password').isLength({min: 6}),
-    body('phone').isMobilePhone(),
-    body('name').isAscii().isLength({min: 3}),
+    checkSchema({
+        'id': {
+            in: ['params'],
+            errorMessage: 'Id must be an integer number',
+            isInt: true,
+            toInt: true
+        },
+        'email': {
+            in: ['body'],
+            errorMessage: 'Email must be contains a valid email',
+            isEmail: true,
+            normalizeEmail: true
+        },
+        'password': {
+            in: ['body'],
+            errorMessage: 'Password must be at least 6 characters',
+            isLength: {
+                options: {min: 6}
+            },
+        },
+        'phone': {
+            in: ['body'],
+            errorMessage: 'Phone must be a valid phone number',
+            isMobilePhone: true
+        },
+        'name': {
+            in: ['body'],
+            errorMessage: 'Name must be at least 3 ascii characters',
+            isAscii: true,
+            isLength: {
+                options: {
+                    min: 3
+                }
+            },
+        }
+    }),
+    // param('id').isInt(),
+    // body('email').isEmail(),
+    // body('password').isLength({min: 6}),
+    // body('phone').isMobilePhone(),
+    // body('name').isAscii().isLength({min: 3}),
     validateParams,
     userController.update
 );
