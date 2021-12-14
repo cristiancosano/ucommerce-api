042fa464cc9b7501e2d89fb68a0c890da83c3afe
cristiancosano <cristiancosano@icloud.com>
2021-12-14 17:24:53 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -13,9 +13,8 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
 app.options('*', cors())
+app.use(cors());
 //app.use(cors);
 app.use(router);
 
